428dfadd414d5a3d4e44b7e455af2c8c503dd74c
tpratama <theopratama@live.com>
2015-11-01 14:19:07 +0700

    soal 3 on progress Thread 1 dan 2 sudah ditambah Thread sudah berjalan, tetapi belum parallel

--- modul 2/soal_3.c
+++ modul 2/soal_3.c
@@ -2,15 +2,40 @@
 #include <stdio.h>
 #include <pthread.h>
 
-void *read_input(void* args){
+
+void *thread1(void* args){
    char* nama_file=(char*)args; 
-   printf("%s",nama_file);
+   char karakter;
+   FILE *in,*out;
+   in=fopen(nama_file,"r");
+   out=fopen("salinan.txt","w");
+   while(fscanf(in,"%c",&karakter)!=EOF){
+   	fprintf(out,"%c",karakter);
+   }
+   fclose(in);
+   fclose(out);
+}
+
+void *thread2(void* args){ 
+   FILE *in,*out;
+   char karakter;
+   
+  in=fopen("salinan.txt","r");
+  out=fopen("salinan2.txt","w");
+  while(fscanf(in,"%c",&karakter)!=EOF){
+	fprintf(out,"%c",karakter);
+  }
+  fclose(in);
+  fclose(out);
 }
 
 int main(){
-   pthread_t t1;
+   pthread_t t1,t2;
    char nama_file[100];
    scanf("%s",nama_file); 
-   pthread_create(&t1,NULL,read_input,(void*) &nama_file[0]);
-   pthread_exit(NULL);
+   pthread_create(&t1,NULL,thread1,(void*) &nama_file[0]);
+   pthread_join(t1,NULL);
+   pthread_create(&t2,NULL,thread2,NULL);
+   pthread_join(t2,NULL);
+   pthread_exit(0);
 }
